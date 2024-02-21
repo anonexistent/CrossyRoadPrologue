debe39e1e1e0c4490f6e48bce00851051300c2a3
Дмитрий К <wwwzlx86@gmail.com>
2024-02-21 18:35:18 +0500

    update (movement, map-geenerator)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,6 +6,10 @@ using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
+    [SerializeField]
+    public int minDistance;
+
+    [HideInInspector]
     Vector3 curPos = new Vector3 (0, 0, 0);
 
     [SerializeField]
@@ -24,40 +28,44 @@ public class MapGenerator : MonoBehaviour
     {
         for(int i = 0; i < maxTerCount; i++)
         {
-            SpawnMap(true);
+            SpawnMap(true, new Vector3(0,0,0));
 
         }
-        maxTerCount = curTers.Count;
+        //maxTerCount = curTers.Count;
     }
 
-    // Update is called once per frame
-    void Update()
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.W))
+    //    {
+    //        SpawnMap(false);
+    //    }
+    //}
+    public void SpawnMap(bool isInitialization, Vector3 pos)
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        Debug.Log(curPos.x -pos.x);
+        if(curPos.x - pos.x < minDistance)
         {
-            SpawnMap(false);
-        }
-    }
-    private void SpawnMap(bool isInitialization)
-    {
-        int temp = Random.Range(0, ters.Count);
-        int terOnMap = Random.Range(1, ters[temp].maxOnMap);
+            int temp = Random.Range(0, ters.Count);
+            int terOnMap = Random.Range(1, ters[temp].maxOnMap);
 
-        for (int i = 0; i < terOnMap; i++)
-        {
-            GameObject t = Instantiate(ters[temp].ter, curPos, Quaternion.identity, mapHolder);
-
-            curTers.Add(t);
-            if(!isInitialization)
+            for (int i = 0; i < terOnMap; i++)
             {
-                if (curTers.Count > maxTerCount)
-                {
-                    Destroy(curTers[0]);
-                    curTers.RemoveAt(0);
-                }
-            }
+                GameObject t = Instantiate(ters[temp].ter, curPos, Quaternion.identity, mapHolder);
 
-            curPos.x++;
+                curTers.Add(t);
+                if (!isInitialization)
+                {
+                    if (curTers.Count > maxTerCount)
+                    {
+                        Destroy(curTers[0]);
+                        curTers.RemoveAt(0);
+                    }
+                }
+
+                curPos.x++;
+            }
         }
 
         //GameObject t = Instantiate(ters[UnityEngine.Random.Range(0, ters.Count)], curPos, Quaternion.identity);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] public MapGenerator MapGenerator; 
+
     [SerializeField] public Animator animator;
 
     // Start is called before the first frame update
@@ -15,10 +17,33 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W)) 
+        float newZ = 0;
+        if (Input.GetKeyDown(KeyCode.W)) 
         {
-            animator.SetTrigger("Space");
-            if(transform.position.z % 1 == 0) transform.Translate(new Vector3(1, 0, 0));
+            animator.SetTrigger("Space");            
+            if (transform.position.z % 1 != 0) newZ = Mathf.Round(transform.position.z) - transform.position.z;
+            DoMove(new Vector3(1, 0, newZ));
         }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            DoMove(new Vector3(0,0,1));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.GetComponentInChildren<Transform>().Rotate(0, 90, 0);
+            DoMove(new Vector3(0, 0, -1));
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            DoMove(new Vector3(-1, 0, 0));
+        }
+    }
+
+    void DoMove(Vector3 world)
+    {
+        animator.SetTrigger("Space");
+        transform.position = (transform.position + world);
+
+        MapGenerator.SpawnMap(false, transform.position);
     }
 }
