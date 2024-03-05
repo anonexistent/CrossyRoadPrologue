3fe80e39f6813416357827d289ab8df1d54f8ebe
anonexistent <wwwzlx86@gmail.com>
2024-03-05 18:13:34 +0500

    fix (anim, mov-t, g. objs)

--- Assets/Scripts/TerrainData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TerrainData", menuName ="TerrainData")]
-public class TerrainData : ScriptableObject
-{
-    public GameObject ter;
-    public int maxOnMap;
-}
